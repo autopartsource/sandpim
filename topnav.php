9112c2014cbe93a23504a65106ee0702c91a583d
Gilmar Palma <palma.gilmar@gmail.com>
2019-11-08 15:44:57 -0500

    Changed Navigation Bar. - Color - DIV to UL - Hover

--- topnav.php
+++ topnav.php
@@ -1,12 +1,11 @@
- <div>
-  <div style="float:left;padding:10px 10px 10px 0px;"><a href="index.php">Dashboard</a></div>
-  <div style="float:left;padding:10px;"><a href="partsIndex.php">Parts</a></div>
-  <div style="float:left;padding:10px;"><a href="appsIndex.php">Applications</a></div>
-  <div style="float:left;padding:10px;"><a href="assetsIndex.php">Assets</a></div>
-  <div style="float:left;padding:10px;"><a href="reportsIndex.php">Reports</a></div>
-  <div style="float:left;padding:10px;"><a href="utilitiesIndex.php">Utilities</a></div>
-  <div style="float:left;padding:10px;"><a href="settings.php">Settings</a></div>
-  <div style="float:left;padding:10px;"><a href="ioIndex.php">Import/Export</a></div>
-  <div style="float:left;padding:10px;"><a href="logout.php">Logout (<?php echo $_SESSION['name'];?>)</a></div>
-  <div style="clear:both;"></div>
- </div>
+<ul class="topnav">
+  <li><a href="index.php">Dashboard</a></li>
+  <li><a href="partsIndex.php">Parts</a></li>
+  <li><a href="appsIndex.php">Applications</a></li>
+  <li><a href="assetsIndex.php">Assets</a></li>
+  <li><a href="reportsIndex.php">Reports</a></li>
+  <li><a href="utilitiesIndex.php">Utilities</a></li>
+  <li><a href="settings.php">Settings</a></li>
+  <li><a href="ioIndex.php">Import/Export</a></li>
+  <li class="right"><a href="logout.php">Logout (<?php echo $_SESSION['name'];?>)</a></li>
+ </ul>
