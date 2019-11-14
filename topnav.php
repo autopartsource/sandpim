9a06d15966b9c915a39f3f87db8cf7b83466a32b
Gilmar Palma <palma.gilmar@gmail.com>
2019-11-14 17:16:53 -0500

    Navbar responsive to pixel width

--- topnav.php
+++ topnav.php
@@ -1,11 +1,25 @@
-<ul class="topnav">
-  <li class="navindex"><a <?php if ($navCategory == 'dashboard') {echo 'class="navActive"';} ?>href="index.php">Dashboard</a></li>
-  <li class="navparts"><a <?php if ($navCategory == 'parts') {echo 'class="navActive"';} ?>href="partsIndex.php">Parts</a></li>
-  <li class="navapplications"><a <?php if ($navCategory == 'applications') {echo 'class="navActive"';} ?>href="appsIndex.php">Applications</a></li>
-  <li class="navassets"><a <?php if ($navCategory == 'assets') {echo 'class="navActive"';} ?>href="assetsIndex.php">Assets</a></li>
-  <li class="navreports"><a <?php if ($navCategory == 'reports') {echo 'class="navActive"';} ?>href="reportsIndex.php">Reports</a></li>
-  <li class="navutilities"><a <?php if ($navCategory == 'utilities') {echo 'class="navActive"';} ?>href="utilitiesIndex.php">Utilities</a></li>
-  <li class="navsettomgs"><a <?php if ($navCategory == 'settings') {echo 'class="navActive"';} ?>href="settings.php">Settings</a></li>
-  <li class="navio"><a <?php if ($navCategory == 'import/export') {echo 'class="navActive"';} ?>href="ioIndex.php">Import/Export</a></li>
-  <li class="navlogout"><a href="logout.php">Logout (<?php echo $_SESSION['name'];?>)</a></li>
-</ul>
+<div class="topnav" id="myTopnav">
+  <a <?php if ($navCategory == 'dashboard') {echo 'class="navActive"';} ?>href="index.php">Dashboard</a>
+  <a <?php if ($navCategory == 'parts') {echo 'class="navActive"';} ?>href="partsIndex.php">Parts</a>
+  <a <?php if ($navCategory == 'applications') {echo 'class="navActive"';} ?>href="appsIndex.php">Applications</a>
+  <a <?php if ($navCategory == 'assets') {echo 'class="navActive"';} ?>href="assetsIndex.php">Assets</a>
+  <a <?php if ($navCategory == 'reports') {echo 'class="navActive"';} ?>href="reportsIndex.php">Reports</a>
+  <a <?php if ($navCategory == 'utilities') {echo 'class="navActive"';} ?>href="utilitiesIndex.php">Utilities</a>
+  <a <?php if ($navCategory == 'settings') {echo 'class="navActive"';} ?>href="settings.php">Settings</a>
+  <a <?php if ($navCategory == 'import/export') {echo 'class="navActive"';} ?>href="ioIndex.php">Import/Export</a>
+  <a href="logout.php">Logout (<?php echo $_SESSION['name'];?>)</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
