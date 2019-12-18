c387ba09d62b95d40d762ceda18fa785b4bece72
Luke Smith <lsmith@autopartsource.com>
2019-12-18 17:26:00 -0500

    part event history

--- appHistory.php
+++ appHistory.php
@@ -2,6 +2,7 @@
 include_once('./class/vcdbClass.php');
 include_once('./class/pcdbClass.php');
 include_once('./class/pimClass.php');
+include_once('./class/userClass.php');
 $navCategory = 'applications';
 
 session_start();
@@ -10,13 +11,36 @@ if (!isset($_SESSION['userid'])) {
     exit;
 }
 
+
+function niceAppAttributes($appattributes) {
+    $vcdb = new vcdb;
+    $niceattributes = array();
+    foreach ($appattributes as $appattribute) {
+        if ($appattribute['type'] == 'vcdb') {
+            $niceattributes[] = array('sequence' => $appattribute['sequence'], 'text' => $vcdb->niceVCdbAttributePair($appattribute), 'cosmetic' => $appattribute['cosmetic']);
+        }
+        if ($appattribute['type'] == 'note') {
+            $niceattributes[] = array('sequence' => $appattribute['sequence'], 'text' => $appattribute['value'], 'cosmetic' => $appattribute['cosmetic']);
+        }
+    }
+    $nicefitmentstring = '';
+    $nicefitmentarray = array();
+    foreach ($niceattributes as $niceattribute) {
+        // exclude cosmetic elements from the compiled list
+        $nicefitmentarray[] = $niceattribute['text'];
+    }
+    return implode('; ', $nicefitmentarray);
+}
+
+
 $vcdb = new vcdb;
 $pcdb = new pcdb;
 $pim = new pim;
+$user=new user;
 
 $appid = intval($_GET['appid']);
 $app = $pim->getApp($appid);
-$history = $pim->getAppEvents($appid, 1000);
+$history = $pim->getAppEvents($appid, 25);
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,7 +52,8 @@ $history = $pim->getAppEvents($appid, 1000);
         <?php include('topnav.php'); ?>
 
         <!-- Header -->
-        <h1>Applications</h1>
+        <h1>History for application <?php echo $appid;?></h1>
+	<h2><?php echo $vcdb->niceMMYofBasevid($app['basevehicleid']) . ' ' . niceAppAttributes($app['attributes']);?></h2>
 
         <div class="wrapper">
             <div class="contentLeft"></div>
@@ -39,7 +64,7 @@ $history = $pim->getAppEvents($appid, 1000);
                 if ($app && count($history)) {
                     echo '<table><tr><th>Date/Time</th><th>User</th><th>Change Description</th><th>OID After Change</th></tr>';
                     foreach ($history as $record) {
-                        echo '<tr><td>' . $record['eventdatetime'] . '</td><td>' . $record['userid'] . '</td><td>' . $record['description'] . '</td><td>' . $record['new_oid'] . '</td></tr>';
+                        echo '<tr><td>' . $record['eventdatetime'] . '</td><td>' . $user->realNameOfUserid($record['userid']) . '</td><td>' . $record['description'] . '</td><td>' . $record['new_oid'] . '</td></tr>';
                     }
                     echo '</table>';
                 } else { // no apps found
