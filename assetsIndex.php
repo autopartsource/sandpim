ce0a7b887a886630dad7dae3f146012dfe4963a6
Gilmar Palma <palma.gilmar@gmail.com>
2020-09-22 14:03:00 -0400

    Fixed ids for card navigation.

--- assetsIndex.php
+++ assetsIndex.php
@@ -55,13 +55,13 @@ $orientationviewcodes=$pcdb->getAssetOrientationViewCodes();
                         <div class="card-body">
                             <ul class="nav nav-tabs" id="myTab" role="tablist">
                                 <li class="nav-item">
-                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#search" role="tab" aria-controls="home" aria-selected="true">Search</a>
+                                    <a class="nav-link active" id="search-tab" data-toggle="tab" href="#search" role="tab" aria-controls="search" aria-selected="true">Search</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#upload" role="tab" aria-controls="profile" aria-selected="false">Upload Asset</a>
+                                    <a class="nav-link" id="upload-tab" data-toggle="tab" href="#upload" role="tab" aria-controls="upload" aria-selected="false">Upload Asset</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#uri" role="tab" aria-controls="contact" aria-selected="false">Upload from URI</a>
+                                    <a class="nav-link" id="uri-tab" data-toggle="tab" href="#uri" role="tab" aria-controls="contact" aria-selected="false">Upload from URI</a>
                                 </li>
                             </ul>
                             <div class="tab-content" id="myTabContent">
