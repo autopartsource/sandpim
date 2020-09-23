01790c469f2cda53b300902f7841721c73477aee
Gilmar Palma <palma.gilmar@gmail.com>
2020-09-23 14:10:41 -0400

    Applied card layout. Moved Show Only Already Selected Types button to header on right. Search result card with scrollbar.

--- pcdbTypeBrowser.php
+++ pcdbTypeBrowser.php
@@ -69,9 +69,6 @@ if(isset($_GET['submit']) && isset($_GET['searchtype']) && isset($_GET['searchte
         <!-- Navigation Bar -->
         <?php include('topnav.php'); ?>
         
-        <!-- Header -->
-        <h3>Favorite PCdb PartTypes</h3>
-        
         <!-- Content Container -->
         <div class="container-fluid padding my-container">
             <div class="row padding my-row">
@@ -82,38 +79,51 @@ if(isset($_GET['submit']) && isset($_GET['searchtype']) && isset($_GET['searchte
                 
                 <!-- Main Content -->
                 <div class="col-xs-12 col-md-8 my-col colMain">
-                    <form method="get">
-                        Part Type Name
-                        <select name="searchtype">
-                            <option value="begins"<?php if($searchtype=='begins'){echo ' selected';}?>>Begins with</option>
-                            <option value="contains"<?php if($searchtype=='contains'){echo ' selected';}?>>Contains</option>
-                            <option value="ends"<?php if($searchtype=='ends'){echo ' selected';}?>>Ends with</option>
-                        </select>
-                        <input type="text" name="searchterm" value="<?php if(isset($_GET['searchterm'])){echo $_GET['searchterm'];}?>"/> 
-                        <input name="submit" type="submit" value="Search"/>
-                     <div style="padding:30px;">
-                         <?php if(count($alltypes)){?>
-                         <table><tr><th>Name</th><th>ID</th><th>Favorite</th></tr>
-                         <?php foreach ($alltypes as $type)
-                          {
-                             $checked=''; if(array_key_exists($type['id'], $idkeyedmytypes)){$checked=' checked';}
-                              echo '<tr><td style="text-align:left;">'.$type['name'].'</td><td>'.$type['id'].'</td>';
-                              echo '<td align="center"><input type="checkbox" id="parttypeid_'.$type['id'].'" name="parttypeid_'.$type['id'].'" onclick="addRemoveType(\''.$type['id'].'\')" name="partcategory_'.$partcategory['id'].'"  '.$checked.'></td>';
-                              echo '</tr>';
-                          }
-                         }
-                         else
-                         { // no results found
-                             if(isset($_GET['submit']))
-                             { // user submitted a search
-                                 echo '<div style="padding:10px;">No Results Found</div>';
-                             }
-                         }
-                         ?>
-                      </table>
-                     </div>
-                    </form>
-                    <a href="pcdbTypeBrowser.php?searchtype=selected&searchterm=&submit=Search">Show only already selected types</a>
+                    <div class="card shadow-sm">
+			<!-- Header -->
+                        <h3 class="card-header text-left">Favorite PCdb PartTypes<div style="float:right"><a class="btn btn-secondary" href="pcdbTypeBrowser.php?searchtype=selected&searchterm=&submit=Search">Show only already selected types</a></div></h3>
+
+                        <div class="card-body">
+                            <form method="get">
+                                Part Type Name
+                                <select name="searchtype">
+                                    <option value="begins"<?php if($searchtype=='begins'){echo ' selected';}?>>Begins with</option>
+                                    <option value="contains"<?php if($searchtype=='contains'){echo ' selected';}?>>Contains</option>
+                                    <option value="ends"<?php if($searchtype=='ends'){echo ' selected';}?>>Ends with</option>
+                                </select>
+                                <input type="text" name="searchterm" value="<?php if(isset($_GET['searchterm'])){echo $_GET['searchterm'];}?>"/> 
+                                <input name="submit" type="submit" value="Search"/>
+                                
+                                <?php if(count($alltypes)){?>
+                                <div class="card">
+                                    <!-- Header -->
+                                    <h6 class="card-header text-left">Search Results</h6>
+
+                                    <div class="card-body scroll">
+                                        <table><tr><th>Name</th><th>ID</th><th>Favorite</th></tr>
+                                            <?php foreach ($alltypes as $type)
+                                             {
+                                                $checked=''; if(array_key_exists($type['id'], $idkeyedmytypes)){$checked=' checked';}
+                                                 echo '<tr><td style="text-align:left;">'.$type['name'].'</td><td>'.$type['id'].'</td>';
+                                                 echo '<td align="center"><input type="checkbox" id="parttypeid_'.$type['id'].'" name="parttypeid_'.$type['id'].'" onclick="addRemoveType(\''.$type['id'].'\')" name="partcategory_'.$partcategory['id'].'"  '.$checked.'></td>';
+                                                 echo '</tr>';
+                                             }
+                                            }
+                                            else
+                                            { // no results found
+                                                if(isset($_GET['submit']))
+                                                { // user submitted a search
+                                                    echo '<div style="padding:10px;">No Results Found</div>';
+                                                }
+                                            }
+                                            ?>
+                                        </table>
+                                    </div>
+                                </div>
+                                    
+                            </form>
+                        </div>
+                    </div>
                 </div>
                 <!-- End of Main Content -->
                 
