4049dbb49eef7b2cbaeff9fa8a0f9207130e1cca
Luke Smith <zerodbu77@gmail.com>
2020-09-29 00:14:49 -0400

    issue list tree

--- showIssue.php
+++ showIssue.php
@@ -13,10 +13,85 @@ if (!isset($_SESSION['userid'])) {
 
 $pim = new pim;
 
-$issue=$pim->getIssueById(intval($_GET['id']));
+$selectedissue=$pim->getIssueById(intval($_GET['id']));
 
-$issueTypes = explode('/',$issue['issuetype']);
+$tree=array();
 
+$issues=$pim->getIssues('%', '%', '%', 9999);
+
+foreach($issues as $issue)
+{
+ $issuebits = explode('/',$issue['issuetype']);
+ 
+ if(count($issuebits))
+ {
+  
+  for($i=0; $i<count($issuebits); $i++)
+  {
+   
+    switch ($i)
+    {
+     case 0:
+         if(!array_key_exists($issuebits[0], $tree))
+         {
+          $tree[$issuebits[0]]=array();
+         }
+         if($i==count($issuebits)-1)
+         {// this is the lowest (least-significant) level of the path
+          $tree[$issuebits[0]][]=$issue;
+         }
+
+         break;
+     case 1:
+         if(!array_key_exists($issuebits[1], $tree[$issuebits[0]]))
+         {
+          $tree[$issuebits[0]][$issuebits[1]]=array();
+         }
+         if($i==count($issuebits)-1)
+         {// this is the lowest (least-significant) level of the path
+          $tree[$issuebits[0]][$issuebits[1]][]=$issue;
+         }
+         
+         break;
+     case 2:
+         if(!array_key_exists($issuebits[2], $tree[$issuebits[0]][$issuebits[1]]))
+         {
+          $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]]=array();
+         }
+         if($i==count($issuebits)-1)
+         {// this is the lowest (least-significant) level of the path
+          $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]][]=$issue;
+         }
+
+         break;
+     case 3:
+         if(!array_key_exists($issuebits[3], $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]]))
+         {
+          $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]][$issuebits[3]]=array();
+         }
+         if($i==count($issuebits)-1)
+         {// this is the lowest (least-significant) level of the path
+          $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]][$issuebits[3]][]=$issue;
+         }
+
+         break;
+     case 4:
+         if(!array_key_exists($issuebits[4], $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]][$issuebits[3]]))
+         {
+          $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]][$issuebits[3]][$issuebits[4]]=array();
+         }
+         if($i==count($issuebits)-1)
+         {// this is the lowest (least-significant) level of the path
+          $tree[$issuebits[0]][$issuebits[1]][$issuebits[2]][$issuebits[3]][$issuebits[4]][]=$issue;
+         }
+
+         break;
+
+     default: break;
+    }
+  }
+ }
+}
 
 ?>
 <!DOCTYPE html>
@@ -49,7 +124,7 @@ $issueTypes = explode('/',$issue['issuetype']);
                                         <div class="collapse" id="submenu1sub1" aria-expanded="false">
                                             <ul class="flex-column nav pl-4">
                                                 <li class="nav-item">
-                                                    <a class="nav-link p-1" href="./showIssue.php?id=<?php echo $issue['id'] ?>"><?php echo $issue['issuekeyalpha'] ?></a>
+                                                    <a class="nav-link p-1" href="./showIssue.php?id=<?php echo $selectedissue['id'] ?>"><?php echo $selectedissue['issuekeyalpha'] ?></a>
                                                 </li>
                                         </div>
                                     </li>
@@ -65,38 +140,86 @@ $issueTypes = explode('/',$issue['issuetype']);
                     </ul>
                     
                 </div>
+
+                <div>
+<?php    
+
+foreach($tree as $key0=>$data0)
+{
+ echo $key0.'<br/>';
+   
+ foreach($data0 as $key1=>$data1)
+ {
+  if(array_key_exists('issuehash', $data1))
+  {// this is an end-node
+   echo '- ' .$data1['description'].'<br/>';
+  }
+  else
+  {// this node contains children
+   echo '- '.$key1.'<br/>';
+  }
+
+  foreach($data1 as $key2=>$data2)
+  {
+   if(array_key_exists('issuehash', $data2))
+   {// this is an end-node
+    echo '- - ' .$data2['description'].'<br/>';
+   }
+   else
+   {// this node contains children
+    echo '- - '.$key2.'<br/>';
+   }
+
+   foreach($data2 as $key3=>$data3)
+   {
+    if(array_key_exists('issuehash', $data3))
+    {// this is an end-node
+     echo '- - - ' .$data3['description'].'<br/>';
+    }
+    else
+    {// this node contains children
+     echo '- - - '.$key3.'<br/>';
+    }
+   }      
+  }     
+ }
+}
+?>
+                </div>
+
+
+
+
                 
                 <!-- Main Content -->
                 <div class="col-xs-12 col-md-10 my-col colMain">
                     <div class="card shadow-sm">
                         <div class="card-header text-left">
-                            <?php echo $issue['issuetype']; ?> Breadcrumb Nav
+                            <?php echo $selectedissue['issuetype']; ?> Breadcrumb Nav
                         </div>
  
-                        <?php print_r($issue);?>
-
                         <div class="card shadow-sm">
                             <!-- Header -->
-                            <h5 class="card-header text-left">Issue ID: <span class="text-info"><?php echo $issue['id']; ?></span></h5>
+                            <h5 class="card-header text-left">Issue ID: <span class="text-info"><?php echo $selectedissue['id']; ?></span></h5>
 
                             <div class="card-body">
                                 <div class="row padding my-row">
                                     <div class="col md-4">
                                         <div class="card shadow-sm">
                                             <!-- Header -->
-                                            <h6 class="card-header text-left">Item - <?php echo $issue['issuekeyalpha']; ?></h6>
+                                            <h6 class="card-header text-left">Item - <?php echo $selectedissue['issuekeyalpha']; ?></h6>
                                         </div>
                                     </div>
                                     <div class="col md-4">
                                         <div class="card shadow-sm">
                                             <!-- Header -->
-                                            <h6 class="card-header text-left">Status - <?php echo $issue['status']; ?></h6>
+                                            <h6 class="card-header text-left">Status - <?php echo $selectedissue['status']; ?></h6>
                                         </div>
                                     </div>
                                     <div class="col md-4">
                                         <div class="card shadow-sm">
                                             <!-- Header -->
-                                            <h6 class="card-header text-left">Source - <?php echo $issue['source']; ?></h6>
+                                            <h6 class="card-header text-left">Source - <?php echo $selectedissue['source']; ?></h6>
                                         </div>
                                     </div>
                                 </div>
@@ -106,7 +229,7 @@ $issueTypes = explode('/',$issue['issuetype']);
                                             <h6 class="card-header text-left">Description</h6>
 
                                             <div class="card-body">
-                                                <?php echo $issue['description']; ?>
+                                                <?php echo $selectedissue['description']; ?>
                                             </div>
                                         </div>
                                     </div>
@@ -115,14 +238,14 @@ $issueTypes = explode('/',$issue['issuetype']);
                                             <h6 class="card-header text-left"> Notes</h6>
 
                                             <div class="card-body">
-                                                <?php echo $issue['notes']; ?>
+                                                <?php echo $selectedissue['notes']; ?>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="card-footer">
-                                Created at <?php echo $issue['issuedatetime']; ?>
+                                Created at <?php echo $selectedissue['issuedatetime']; ?>
                             </div>
                         </div>
                         
