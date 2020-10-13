8221efd0f16c75ae33963eb0b880585118c0d2ba
Gilmar Palma <palma.gilmar@gmail.com>
2020-10-13 12:23:08 -0400

    Added tooltip

--- includes/header.php
+++ includes/header.php
@@ -6,9 +6,13 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <script>
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover({
-        placement : 'top'
-    });
-});
+//$(document).ready(function(){
+//    $('[data-toggle="popover"]').popover({
+//        placement : 'top'
+//    });
+//});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 </script>
