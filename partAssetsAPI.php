c271cd43859728d1cec73d7c72a9570597f12b38
Luke Smith <lsmith@autopartsource.com>
2025-08-06 18:36:20 -0400

    made the part-assets API a little more tolerant of invalid partnumbers

--- partAssetsAPI.php
+++ partAssetsAPI.php
@@ -5,6 +5,7 @@ include_once('./class/assetClass.php');
 include_once('./class/configGetClass.php');
 
 // to automate the interaction with external systems looking to answer the question: "what are the assets for this part"
+//
 
 $pim= new pim;
 $logs = new logs;
@@ -18,6 +19,7 @@ if(!$pim->allowedHost($_SERVER['REMOTE_ADDR']))
 $configGet = new configGet();
 
 $enabled=$configGet->getConfigValue('partAssetAPIenabled','no');
+$assets_linked_to_item=array();
 
 if($enabled=='yes')
 {
@@ -26,14 +28,15 @@ if($enabled=='yes')
  if($part)
  {
   $assets_linked_to_item= $asset->getAssetsConnectedToPart($part['partnumber']);
-  echo json_encode($assets_linked_to_item);
+  $logs->logSystemEvent('INFO', 0, 'partAssetAPI was queried with for partnumber ['.$_GET['partnumber'].'] by '.$_SERVER['REMOTE_ADDR']);  
  }
  else
  {// part number given is not valid
   $logs->logSystemEvent('SECURITY', 0, 'partAssetAPI was queried with an invalid partnumber ['.$_GET['partnumber'].'] by '.$_SERVER['REMOTE_ADDR']);
  }
+ echo json_encode($assets_linked_to_item);
 }
 else
 {
  $logs->logSystemEvent('SECURITY', 0, 'partAssetAPI.php was queried by '.$_SERVER['REMOTE_ADDR'].', but is not enabled. Set config value partAssetAPIenabled to yes to enable.');    
-}?>
+}
